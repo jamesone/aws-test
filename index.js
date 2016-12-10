a022cff19446eecd64038c24ecb6e1b7d12ad0a0
james wainwright <jameswain10@gmail.com>
2016-12-10 14:07:35 +1100

    Change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('The Docker container is working (1)')
+  res.send('The Docker container is working (2) - CHANGE')
 })
 
 app.listen(3000, function () {
